b840c9764a99dc5506b248ee47b594e7fd91e375
elenaJO <ele.lucero1993@gmail.com>
2018-03-03 00:49:14 -0500

    creando componente

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+var generateColor = number => Math.floor(Math.random() * (number + 1)); 
+var color = 'rgb(' + generateColor(255) + ',' + generateColor(255) + ',' + generateColor(255) + ')';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Randon box React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="box" style={{background:color}}>
+          <p className="letter-box">Box</p>
+        </div>
       </div>
     );
   }
